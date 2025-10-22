ecc328095bf6196b5fbb3f2da0edc185112fd3c0
lovinglifemediawebdev <80adev@gmail.com>
2025-10-22 23:20:43 +0800

    Update bio.ts
    Changed from descending order

--- src/content/bio.ts
+++ src/content/bio.ts
@@ -1,9 +1,9 @@
 export const BIO = [
   {
-    date: "2016 - 2019",
-    title: "Completed the Information Technology program at Ateneo de Davao University",
-    description:
-      "Davao City, Philippines",
+    date: "2024 - Today",
+    title: "Owner and Lead Developer at 80A.dev",
+    description: "Building modern fullstack web applications and CMS-integrated sites for global clients under my own brand. Focusing on Responsive and SEO-driven design.",
+    location: "Davao City, Philippines",
   },
   {
     date: "January 2020 - February 2024",
@@ -11,10 +11,10 @@ export const BIO = [
     description:
       "Implemented new technologies using Automation, which improved data tracking and analysis processes, increasing efficiency by as much as 80%. Also assisted day to day support with various administrative task",
   },
- {
-  date: "2024 - Today",
-  title: "Owner and Lead Developer at 80A.dev",
-  description: "Building modern fullstack web applications and CMS-integrated sites for global clients under my own brand. Focusing on Responsive and SEO-driven design.",
-  location: "Davao City, Philippines",
-},
+  {
+    date: "2016 - 2019",
+    title: "Completed the Information Technology program at Ateneo de Davao University",
+    description:
+      "Davao City, Philippines",
+  },
 ]
