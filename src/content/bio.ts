b7c1476624d369cb892032aa65026f7beab46bad
lovinglifemediawebdev <80adev@gmail.com>
2025-10-22 23:44:49 +0800

    Update bio.ts

--- src/content/bio.ts
+++ src/content/bio.ts
@@ -1,12 +1,12 @@
 export const BIO = [
   {
-    date: "2024 - Today",
+    date: "2025 - Today",
     title: "Owner and Lead Developer at 80A.dev",
     description: "Building modern fullstack web applications and CMS-integrated sites for global clients under my own brand. Focusing on Responsive and SEO-driven design.",
     location: "Davao City, Philippines",
   },
   {
-    date: "January 2020 - February 2024",
+    date: "January 2020 - February 2025",
     title: "Worked at Lava Automation as Virtual Assistant and Digital Assistant",
     description:
       "Implemented new technologies using Automation, which improved data tracking and analysis processes, increasing efficiency by as much as 80%. Also assisted day to day support with various administrative task",
